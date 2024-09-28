7335b3cc1ffdd51e49c9d7546ac2ba1e55c2942f
codexman02 <hamzahusain02@gmail.com>
2024-09-29 00:09:53 +0530

    added mongostring

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 export async function dbConnect(){
-    let url=process.env.MONGODB_URI;
+    let url;
+    if(process.env.NODE_ENV=='production'){
+        url=process.env.MONGODB_URI
+    }else if(process.env.NODE_ENV=='development'){
+        url='mongodb://localhost:27017/hamchat'
+    }
 
     // let connect:Promise<typeof mongoose>|null=null;
     let connect;
