b574b1814984ce12c2851b57ef94275263b80e87
codexman02 <hamzahusain02@gmail.com>
2024-09-29 01:26:31 +0530

    mongoatlas connect

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 export async function dbConnect(){
     let url;
+    console.log(process.env.MONGODB_URI)
     if(process.env.NODE_ENV=='production'){
-        url=process.env.MONGODB_URI
+        url=process.env.MONGODB_URI?.toString();
     }else if(process.env.NODE_ENV=='development'){
         url='mongodb://localhost:27017/hamchat'
+        // url=process.env.MONGODB_URI?.toString()
     }
 
     // let connect:Promise<typeof mongoose>|null=null;
