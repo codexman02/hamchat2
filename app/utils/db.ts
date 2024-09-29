8879f15db484e64c0c401382b56f047af924cc81
codexman02 <hamzahusain02@gmail.com>
2024-09-29 13:25:28 +0530

    middleware dbconnection

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -2,17 +2,15 @@ import mongoose from "mongoose";
 
 export async function dbConnect(){
     let url;
-    console.log(process.env.MONGODB_URI)
     if(process.env.NODE_ENV=='production'){
         url=process.env.MONGODB_URI?.toString();
     }else if(process.env.NODE_ENV=='development'){
-        url=process.env.MONGODB_URI?.toString()
+        url=process.env.MONGODB_URI?.toString();
         // url=process.env.MONGODB_URI?.toString()
     }
 
     // let connect:Promise<typeof mongoose>|null=null;
     let connect;
-
     if(!connect){
         
         if(url){
